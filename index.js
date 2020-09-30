bfc86d1fd707be778dea9aafad3b7df5df7e52bc
JirawatChairongsri <42440557+JirawatChairongsri@users.noreply.github.com>
2020-09-30 15:43:22 +0700

    1

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 var express = require('express');
 var app = express();
 
-app.get('/', function(req, res) {
+app.get('/ping', function(req, res) {
     res.send({ping:'hello this is server and I am alive!'});
+});
+
+app.get('/ping/:id', function(req, res) {
+    res.send({ping:'hello this is server and I am got '+req.params.id});
 });
 
 app.listen(3006);
