1b9edd3138a3843bd63b359e6d82ae17014f7e60
JirawatChairongsri <42440557+JirawatChairongsri@users.noreply.github.com>
2020-09-30 14:51:47 +0700

    1

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ var http = require('http');
 
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World!');
+  res.end('Jirawat Chairong 60030016');
 }).listen(3006);
