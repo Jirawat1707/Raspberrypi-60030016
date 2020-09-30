fd259b65939a57d858f088ab74d071db77c7a68f
JirawatChairongsri <42440557+JirawatChairongsri@users.noreply.github.com>
2020-09-30 15:35:08 +0700

    1

--- index.js
+++ index.js
@@ -5,9 +5,5 @@ app.get('/', function(req, res) {
     res.send({ping:'hello this is server and I am alive!'});
 });
 
-app.get('/id', function(req, res) {
-    res.send({ping:'hello this is server and I am got '+req.params.id});
-});
-
 app.listen(3006);
 console.log('Listening on port 3006');
